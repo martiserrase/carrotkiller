ffc75400a89267ab348b4695eebfc51f41785b09
Martí Serratosa <serratosa.marti@gmail.com>
2022-11-16 19:32:47 +0100

    game, statistics, menu and other templates

--- carrotkiller/app/src/main/java/com/example/interaction/LoginActivity.java
+++ carrotkiller/app/src/main/java/com/example/interaction/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dead);
+        setContentView(R.layout.activity_congrats);
 
         /*TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
