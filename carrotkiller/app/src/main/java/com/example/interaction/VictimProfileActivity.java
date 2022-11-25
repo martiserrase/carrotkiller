9a1ff1d7543aacfd8184579a4ca29689db069dea
luciacp1312 <80093487+luciacp1312@users.noreply.github.com>
2022-11-25 20:28:40 +0100

    read/write from the database

--- carrotkiller/app/src/main/java/com/example/interaction/VictimProfileActivity.java
+++ carrotkiller/app/src/main/java/com/example/interaction/VictimProfileActivity.java
@@ -1,12 +1,21 @@
 package com.example.interaction;
 
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class VictimProfileActivity extends AppCompatActivity {
 
@@ -34,5 +43,18 @@ public class VictimProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+/*
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Profile").child("Name");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data = snapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });*/
     }
 }
