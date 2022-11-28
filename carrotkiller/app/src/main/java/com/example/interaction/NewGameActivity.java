49a50f82d6bd4842a8d0f89c3ebd9275b27a0856
Martí Serratosa <serratosa.marti@gmail.com>
2022-11-28 10:53:15 +0100

    menu settings configured on new game activity

--- carrotkiller/app/src/main/java/com/example/interaction/NewGameActivity.java
+++ carrotkiller/app/src/main/java/com/example/interaction/NewGameActivity.java
@@ -5,11 +5,15 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -99,10 +103,7 @@ public class NewGameActivity extends AppCompatActivity {
                 initial_date.setText(dayOfMonth + "/" + month + "/" + year);
             }
         };
-        new DatePickerDialog(NewGameActivity.this, dateSetListener,
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)).show();
+        new DatePickerDialog(NewGameActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
     private void showInitialTimeDialog(EditText initial_time) {
@@ -115,9 +116,7 @@ public class NewGameActivity extends AppCompatActivity {
                 initial_time.setText(hourOfDay + ":" + minute);
             }
         };
-        new TimePickerDialog(NewGameActivity.this, timeSetListener,
-                calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), false).show();
+        new TimePickerDialog(NewGameActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
     }
 
     private void showFinalDateDialog(EditText final_date) {
@@ -131,10 +130,7 @@ public class NewGameActivity extends AppCompatActivity {
                 final_date.setText(dayOfMonth + "/" + month + "/" + year);
             }
         };
-        new DatePickerDialog(NewGameActivity.this, dateSetListener,
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)).show();
+        new DatePickerDialog(NewGameActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 
     private void showFinalTimeDialog(EditText final_time) {
@@ -147,8 +143,36 @@ public class NewGameActivity extends AppCompatActivity {
                 final_time.setText(hourOfDay + ":" + minute);
             }
         };
-        new TimePickerDialog(NewGameActivity.this, timeSetListener,
-                calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), false).show();
+        new TimePickerDialog(NewGameActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this, "SubItem 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this, "SubItem 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
