cf58a07803c0be15d3ff1be6f14f0a8306311659
luciacp1312 <80093487+luciacp1312@users.noreply.github.com>
2022-11-30 01:06:52 +0100

    Bottom Navigation Bar. First attempt

--- carrotkiller/app/src/main/java/com/example/interaction/NavActivity.java
+++ carrotkiller/app/src/main/java/com/example/interaction/NavActivity.java
@@ -1,14 +1,43 @@
 package com.example.interaction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.interaction.databinding.ActivityMainBinding;
+
 public class NavActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->  {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.victim:
+                    replaceFragment(new VictimFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
